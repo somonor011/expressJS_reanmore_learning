fbc23bc69e94a069f01b94e78ca4e39787b403f4
somonor011 <somonorhong011@gmail.com>
2024-11-15 19:19:31 +0700

    respone as html

--- app.js
+++ app.js
@@ -1,24 +1,28 @@
 const express = require("express");
+const path = require("path");
+
 
 const app = express();
 
+// respone as HTML
 app.get("/", (req , res)=>{
-    res.send("This is home page!");
+    res.sendFile(path.resolve(__dirname,"pages/index.html"))
 })
 
 app.get("/about", (req , res)=>{
     res.send("This is about page!");
 })
 
+
 // when user go to page that none , show page not found
-// app.all("*/" , (req,res)=>{ // all = post and get
-//     res.status(404).send("Page not found");
-// })
+app.all("*/" , (req,res)=>{ // all = post and get
+    res.status(404).send("Page not found");
+})
 
 // another way
-app.use("*", (req , res)=>{
-    res.status(404).send("Page not found")
-})
+// app.use("*", (req , res)=>{
+//     res.status(404).send("Page not found")
+// })
 
 app.listen(3000 , ()=> {
     console.log("Server is listen on port 3000");
