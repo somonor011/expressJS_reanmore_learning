f9e413261334ec8f76c9da2fc0d1ceb237c1b8b9
somonor011 <somonorhong011@gmail.com>
2024-11-15 19:09:25 +0700

    page not found

--- app.js
+++ app.js
@@ -10,6 +10,11 @@ app.get("/about", (req , res)=>{
     res.send("This is about page!");
 })
 
+// when user go to page that none , show page not found
+app.all("*/" , (req,res)=>{ // all = post and get
+    res.status(404).send("Page not found");
+})
+
 app.listen(3000 , ()=> {
     console.log("Server is listen on port 3000");
 })
