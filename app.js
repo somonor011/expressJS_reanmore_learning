902064b481b2c13f1fd1cb817ed418d9e8d6e73f
somonor011 <somonorhong011@gmail.com>
2024-11-15 19:12:57 +0700

    page not found another way

--- app.js
+++ app.js
@@ -11,8 +11,13 @@ app.get("/about", (req , res)=>{
 })
 
 // when user go to page that none , show page not found
-app.all("*/" , (req,res)=>{ // all = post and get
-    res.status(404).send("Page not found");
+// app.all("*/" , (req,res)=>{ // all = post and get
+//     res.status(404).send("Page not found");
+// })
+
+// another way
+app.use("*", (req , res)=>{
+    res.status(404).send("Page not found")
 })
 
 app.listen(3000 , ()=> {
